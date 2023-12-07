9432f3ba7706d455cce578148ee3bbf09b069dc5
Ondřej Schlaichert <schlaichert.o@gmail.com>
2023-12-07 22:20:33 +0100

    Added basic evaluator functionality

--- Program.cs
+++ Program.cs
@@ -1,9 +1,12 @@
-﻿using TimetableGenerator.Helpers;
+﻿using TimetableGenerator;
+using TimetableGenerator.Helpers;
 using TimetableGenerator.TimetableGeneration;
 using TimetableGenerator.TimetableGeneration.Entities;
 
 for (var i = 0; i < 10; i++)
 {
-    ColoredConsole.WriteArray(Generator.GeneratePossibleTimetable().ToColoredStringList());
+    var timetable = Generator.GeneratePossibleTimetable();
+    ColoredConsole.WriteArray(timetable.ToColoredStringList());
+    ColoredConsole.WriteLine("Rating: " + Evaluator.EvaluateTimetable(timetable), ConsoleColor.Green);
     ColoredConsole.WriteLine();
 }
--- TimetableGeneration/Evaluator.cs
+++ TimetableGeneration/Evaluator.cs
@@ -1,6 +1,57 @@
-namespace TimetableGenerator;
+using TimetableGenerator.TimetableGeneration.Entities;
 
-public class Evaluator
+namespace TimetableGenerator.TimetableGeneration;
+
+public abstract class Evaluator
 {
-    
+    public static int EvaluateTimetable(TimetableEntity timetable)
+    {
+        var score = 10000;
+
+        score += RateTakenTimetableFields(timetable);
+        score += PenaliseRepeatedNonconsecutiveLessons(timetable);
+
+        return score;
+    }
+
+    private static int RateTakenTimetableFields(TimetableEntity timetable)
+    {
+        var score = 0;
+        foreach (var day in timetable.Days.Values)
+        {
+            if (day[8] != null) score -= 200;
+            else if (day[6] != null) score -= 50;
+            else if (day[7] != null) score -= 100;
+        }
+
+        // Special Friday penalty for afternoon lessons:
+        var friday = timetable.Days.Values.ElementAt(4);
+        if (friday[8] != null) score -= 500;
+        else if (friday[6] != null) score -= 200;
+        else if (friday[7] != null) score -= 300;
+
+        return score;
+    }
+
+    private static int PenaliseRepeatedNonconsecutiveLessons(TimetableEntity timetable)
+    {
+        var score = 0;
+
+        foreach (var day in timetable.Days.Values)
+        {
+            LessonEntity? lastLesson = null;
+            List<LessonEntity> lessonsHad = new();
+
+            foreach (var lesson in day)
+            {
+                if (lesson == null) continue;
+                if (lessonsHad.Contains(lesson) && lastLesson != lesson) score -= 150;
+                
+                lessonsHad.Add(lesson);
+                lastLesson = lesson;
+            }
+        }
+
+        return score;
+    }
 }
